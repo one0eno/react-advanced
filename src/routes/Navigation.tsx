34dd406bb93a2b3d91ae74fc39ad3f376d4d054b
jorge <jorge.arranz@gmail.com>
2021-12-16 22:56:46 +0100

    Componentes lazy agregados

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Navigate } from "react-router-dom";
 import { Routes, Route, NavLink } from "react-router-dom";
 import logo from "../logo.svg";
+
+import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+
 export const Navigation = () => {
   return (
     <BrowserRouter>
@@ -10,36 +13,36 @@ export const Navigation = () => {
           <ul>
             <li>
               <NavLink
-                to="/home"
+                to="/lazy1"
                 className={({ isActive }) => (isActive ? "nav-active" : "")}
               >
-                Home
+                lazy1
               </NavLink>
             </li>
             <li>
               <NavLink
-                to="/about"
+                to="/lazy2"
                 className={({ isActive }) => (isActive ? "nav-active" : "")}
               >
-                About
+                lazy2
               </NavLink>
             </li>
             <li>
               <NavLink
-                to="/users"
+                to="/lazy3"
                 className={({ isActive }) => (isActive ? "nav-active" : "")}
               >
-                Users
+                lazy3
               </NavLink>
             </li>
           </ul>
         </nav>
 
         <Routes>
-          <Route path="/about" element={<h1>About page</h1>} />
-          <Route path="/users" element={<h1>user page</h1>} />
-          <Route path="/home" element={<h1>Hombe page</h1>} />
-          <Route path="/*" element={<Navigate to="/home" replace />} />
+          <Route path="/lazy1" element={<LazyPage1 />} />
+          <Route path="/lazy2" element={<LazyPage2 />} />
+          <Route path="/lazy3" element={<LazyPage3 />} />
+          <Route path="/*" element={<Navigate to="/lazy1" replace />} />
         </Routes>
       </div>
     </BrowserRouter>
